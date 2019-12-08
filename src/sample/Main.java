9612f603d6e59b129a7ea3b3d0ce778d09d841db
per4ik94 <56968816+per4ik94@users.noreply.github.com>
2019-12-08 21:18:28 +0100

    Code Kommentiert und JavaDoc erstellt.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,14 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/***
+ * Startet die Applikation.
+ *
+ * @author Sergej Cerkasin m17574
+ * @version HA_E1.2
+ * @since 2019/12/31
+ */
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));  //Quelle der XML
+        primaryStage.setTitle("Hello World");// Fenster Titel.
+        primaryStage.setScene(new Scene(root, 300, 275)); // Main Fenster Groesse festlegen
+        primaryStage.show(); // Startet die Applikation
     }
 
 
--- src/sample/SampleController.java
+++ src/sample/SampleController.java
@@ -3,10 +3,22 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
+/***
+ * Kontrolle fuer die GUI interaktionen.
+ *
+ * @author Sergej Cerkasin m17574
+ * @version HA_E1.2
+ * @since 2019/12/31
+ */
+
 public class SampleController {
 
     public Label helloWorld;
 
+    /**
+     * Setzt dem Label ein Text.
+     * @param actionEvent wird erwartet.
+     */
     public void sayHelloWorld(ActionEvent actionEvent) {
         helloWorld.setText("Hello World!");
     }
